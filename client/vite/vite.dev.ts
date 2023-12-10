883cefab2643324493b8cb3d3887fc77972b0408
leia uwu <leia@tutamail.com>
2023-12-10 13:23:17 -0300

    dasfklasd

--- client/vite/vite.dev.ts
+++ client/vite/vite.dev.ts
@@ -6,7 +6,8 @@ const config: UserConfig = {
     server: {
         port: 3000,
         strictPort: true,
-        host: "0.0.0.0"
+        host: "0.0.0.0",
+
     },
     preview: {
         port: 3000,
